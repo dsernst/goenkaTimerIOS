d7790e5bd41031a51cb73d87759acac4f305d489
David Ernst <git@dsernst.com>
2015-09-29 00:22:23 -0700

    Add basic time picker

--- index.ios.js
+++ index.ios.js
@@ -10,18 +10,43 @@ var {
   StyleSheet,
   Text,
   View,
+  PickerIOS,
 } = React;
 
+var PickerItemIOS = PickerIOS.Item;
+
+var times = ['2', '5', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55', '60']
+
 var goenkaTimerIOS = React.createClass({
+  getInitialState: function() {
+    return {
+      carMake: 'cadillac',
+      modelIndex: 6,
+    };
+  },
+
+
   render: function() {
+    // var selectionString = make.name + ' ' + make.models[this.state.modelIndex];
     return (
       <View style={styles.container}>
+
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          How long would you like to sit?
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
+        <PickerIOS
+          selectedValue={this.state.carMake}
+          onValueChange={(carMake) => this.setState({carMake, modelIndex: 0})}>
+          {times.map((carMake) => (
+            <PickerItemIOS
+              key={carMake}
+              value={carMake}
+              label={carMake + ' minutes'}
+              />
+            )
+          )}
+        </PickerIOS>
+
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
@@ -35,7 +60,7 @@ var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    // alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
